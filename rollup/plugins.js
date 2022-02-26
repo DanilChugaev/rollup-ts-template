51af74bb2e0121f63fe3a31463727bea0f2ff521
danilchugaev <dmchugaev@gmail.com>
2022-02-27 00:40:04 +0700

    add parse html

--- rollup/plugins.js
+++ rollup/plugins.js
@@ -1,6 +1,8 @@
 import typescript from '@rollup/plugin-typescript';
+import html from '@web/rollup-plugin-html';
 import { terser } from 'rollup-plugin-terser';
 import clear from 'rollup-plugin-clear';
+
 import { DIST_DIR, isDev } from './utils';
 
 export default [
@@ -8,8 +10,16 @@ export default [
     targets: [DIST_DIR],
     watch: true,
   }),
+
   typescript({
     tsconfig: './tsconfig.json',
   }),
+
   !isDev ? terser() : null,
+
+  html({
+    input: ['src/index.html'],
+    extractAssets: false,
+    minify: !isDev,
+  }),
 ];
